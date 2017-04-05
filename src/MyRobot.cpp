db7ad3f4f4c090178550f82666126b004a42c0b1
Intimitrons <Intimitrons@10.13.176.166>
2017-04-04 19:03:57 -0600

    Remove dead code, Correct motor assugnments

--- src/MyRobot.cpp
+++ src/MyRobot.cpp
@@ -31,86 +31,23 @@ class MyRobot : public SampleRobot
         std::unique_ptr<Talon> MotorLeft2;
         std::unique_ptr<Talon> MotorRight1;
         std::unique_ptr<Talon> MotorRight2;
-        std::unique_ptr<Victor> ArmHinge;
-        std::unique_ptr<Victor> LiftWinch;
-        std::unique_ptr<Victor> PositioningWinch;
-        std::unique_ptr<Victor> Whapper;
-        std::unique_ptr<DigitalInput> Limit;
+        std::unique_ptr<Talon> MotorClimber;
         std::unique_ptr<frc::XboxController> Drive;
-        std::unique_ptr<Joystick> Controls;
-        std::unique_ptr<frc::Spark> TestMotor;
 
     public:
         void RobotInit()
         {
             /*Robot initialization function*/
-            MotorLeft1.reset(new Talon(0));
-            MotorLeft2.reset(new Talon(1));
-            MotorRight1.reset(new Talon(2));
-            MotorRight2.reset(new Talon(3));
-            ArmHinge.reset(new Victor(8));
-            LiftWinch.reset(new Victor(9));
-            PositioningWinch.reset(new Victor(6));
-            Whapper.reset(new Victor(7));
-            Limit.reset(new DigitalInput(0)); //Added as an example, DigitalInput class not tested
+        	MotorRight1.reset(new Talon(0));
+        	MotorRight2.reset(new Talon(1));
+        	MotorLeft1.reset(new Talon(2));
+        	MotorLeft2.reset(new Talon(3));
+	        MotorClimber.reset(new Talon(4));
             //stick = new Joystick(driveChannel);
             Drive.reset(new frc::XboxController(0));//Driving controller
-            Controls.reset(new Joystick(1, 6, 10));//Winch and arm controller
-            TestMotor.reset(new Spark(5));
+
             CameraServer::GetInstance()->StartAutomaticCapture(0);
             CameraServer::GetInstance()->StartAutomaticCapture(1);
-        }
-
-        void armTest()
-        {
-            bool rightBumper= Controls->GetRawButton(6);
-            bool leftBumper = Controls->GetRawButton(5);
-            bool xButton = Controls->GetRawButton(3);
-            bool yButton = Controls->GetRawButton(4);
-            bool aButton = Controls->GetRawButton(1);
-            bool bButton = Controls->GetRawButton(2);
-
-            float leftTrigger=Drive->GetRawAxis(2);
-            float rightTrigger=Drive->GetRawAxis(3);
-            double magnitude = .5;
-
-            float armHingeSpeed;
-            float liftWinchSpeed;
-            float posWinchSpeed;
-            float whapperSpeed;
-
-            if (rightBumper)
-                armHingeSpeed = magnitude;
-            else if (leftBumper)
-                armHingeSpeed = -magnitude;
-            else
-                armHingeSpeed = 0;
-
-            if (xButton)
-                liftWinchSpeed = magnitude;
-            else if (yButton)
-                liftWinchSpeed = -magnitude;
-            else
-                liftWinchSpeed = 0;
-
-            if (aButton)
-                posWinchSpeed = magnitude*2;
-            else if (bButton)
-                posWinchSpeed = -magnitude*1.5;
-            else
-                posWinchSpeed = 0;
-
-            if (leftTrigger>=0.5)
-                whapperSpeed = magnitude;
-            else if (rightTrigger>=0.5)
-                whapperSpeed = -magnitude;
-            else
-                whapperSpeed = 0;
-
-            ArmHinge->Set(armHingeSpeed);
-            LiftWinch->Set(liftWinchSpeed);
-            PositioningWinch->Set(posWinchSpeed);
-            Whapper->Set(whapperSpeed);
         }
 
         void arcadeDrive()
@@ -118,7 +55,7 @@ class MyRobot : public SampleRobot
             float straight=Drive->GetRawAxis(1);
             float turn=Drive->GetRawAxis(4);
             bool turbo=Drive->GetRawButton(5); //Left shoulder button acts as turbo
-            //double leftTrigger = Drive->GetTriggerAxis(frc::GenericHID::JoystickHand::kLeftHand);
+            double leftTrigger = Drive->GetTriggerAxis(frc::GenericHID::JoystickHand::kLeftHand);
 
             //Deadband logic
 
@@ -149,33 +86,7 @@ class MyRobot : public SampleRobot
             MotorLeft2->Set(left);
             MotorRight1->Set(right);
             MotorRight2->Set(right);
-           // TestMotor->Set(leftTrigger);
-        }
-
-        void tankDrive()
-        {
-            float left=Drive->GetRawAxis(1)*-1; //Apparently one side is wired in opposition to the other
-            float right=Drive->GetRawAxis(5);
-            bool turbo=Drive->GetRawButton(5); //Left shoulder button acts as turbo
-            //Inputs from Control controller
-            //Inputs on the robot itself
-
-            //Deadband logic
-            if (std::abs(right)<=0.2)
-                right=0;
-            if (std::abs(left)<=0.2)
-                left=0;
-            if (!turbo) //If turbo is on, it scales from -1 through 1, no slowdown. Turbo off will slow down operation as below
-            {
-                left=std::pow(left,3)*0.5;
-                right=std::pow(right,3)*0.5;
-            }
-            else
-                std::printf("Turbo!"); //Screams "Turbo!" so we know it's going fast :P
-            MotorLeft1->Set(left);
-            MotorLeft2->Set(left);
-            MotorRight1->Set(right);
-            MotorRight2->Set(right);
+            MotorClimber->Set(leftTrigger*0.5);
         }
 
         void Autonomous()
@@ -202,30 +113,14 @@ class MyRobot : public SampleRobot
         {
             while (IsOperatorControl() && IsEnabled())
             {
-                armTest();
                 arcadeDrive();
-                // tankDrive()
-                Wait(0.02); // wait for a motor update time
+            	Wait(0.02); // wait for a motor update time
             }
         }
 
         void Test()
         {
             std::printf("entering test mode");
-
-            //std::shared_ptr<USBCamera> camera(new USBCamera(USBCamera::kDefaultCameraName, false));
-            //CameraServer * cServer = CameraServer::GetInstance();
-            //camera->StartCapture();
-            //try
-            //{
-            //    cServer->StartAutomaticCapture(camera);
-            //}
-            //catch (...)
-            //{
-            //    camera = nullptr;
-            //}
-
-            //while (IsTest())
         }
 };
 
